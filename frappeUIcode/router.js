d3b8626da2f889a3b950311f0660fff24aed1476
Diya050 <124448340+Diya050@users.noreply.github.com>
2023-07-21 15:03:19 +0530

    Update router.js

--- frappeUIcode/router.js
+++ frappeUIcode/router.js
@@ -1,25 +1,45 @@
-// Import Vue Router functions for creating the router and web history
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
+import { session } from './data/session'
+import { userResource } from '@/data/user'
 
-// Define the routes for the application
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/pages/Home.vue'), // Lazy loading of the Home component
+    component: () => import('@/pages/Home.vue'),
+  },
+  {
+    name: 'Login',
+    path: '/account/login',
+    component: () => import('@/pages/Login.vue'),
   },
   {
     path: '/actions/:name',
     component: () => import('@/pages/ActionDetails.vue'), // Lazy loading of the ActionDetails component
     props: true, // Pass route parameters as props to the component
   },
-];
+]
 
-// Create the router instance
 let router = createRouter({
-  history: createWebHistory('/frontend'), // Set up web history mode with a base URL of '/frontend'
-  routes, // Pass the defined routes to the router instance
-});
+  history: createWebHistory('/frontend'),
+  routes,
+})
 
-// Export the router instance to be used in the app
-export default router;
+router.beforeEach(async (to, from, next) => {
+  let isLoggedIn = session.isLoggedIn
+  try {
+    await userResource.promise
+  } catch (error) {
+    isLoggedIn = false
+  }
+
+  if (to.name === 'Login' && isLoggedIn) {
+    next({ name: 'Home' })
+  } else if (to.name !== 'Login' && !isLoggedIn) {
+    next({ name: 'Login' })
+  } else {
+    next()
+  }
+})
+
+export default router

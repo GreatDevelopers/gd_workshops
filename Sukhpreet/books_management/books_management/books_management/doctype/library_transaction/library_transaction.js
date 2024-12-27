b985ed4eece96a08ce2a075861678c5de764754f
SUKHPREET SINGH <82471879+sukhlotey@users.noreply.github.com>
2024-12-27 11:48:36 +0530

    Delete Sukhpreet/books_management directory

--- Sukhpreet/books_management/books_management/books_management/doctype/library_transaction/library_transaction.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Sukhpreet Singh and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Library Transaction", {
-// 	refresh(frm) {
-
-// 	},
-// });
